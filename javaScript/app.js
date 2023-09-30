602053a8a58afd7692bde9bfe40e6892f27e1610
Yonko-Kunal <128897093+Yonko-Kunal@users.noreply.github.com>
2023-09-30 18:38:46 +0530

    update 1.3

--- javaScript/app.js
+++ javaScript/app.js
@@ -9,9 +9,10 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data);
+    // console.log(data);
 
-    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".loc-country .city").innerHTML = data.name + " , ";
+    document.querySelector(".loc-country .country").innerHTML = data.sys.country;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&deg;";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
